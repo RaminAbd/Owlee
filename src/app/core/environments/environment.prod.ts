c7e267ca069a2617855c75e639bb7f3cbaa94e15
RaminAbd <rmnabdulaev@gmail.com>
2026-03-07 13:51:13 +0400

    release to real env

--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -1,6 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://owlee-api-test.azurewebsites.net/api/',
-  url: 'https://owlee-api-test.azurewebsites.net/',
-  webUrl:'https://owlee-test.azurewebsites.net/'
+  apiUrl: 'https://owlee-api.azurewebsites.net/api/',
+  url: 'https://owlee-api.azurewebsites.net/',
+  webUrl:'https://owlee.tech/'
 };
+
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://owlee-api-test.azurewebsites.net/api/',
+//   url: 'https://owlee-api-test.azurewebsites.net/',
+//   webUrl:'https://owlee-test.azurewebsites.net/'
+// };
--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://owlee-api-test.azurewebsites.net/api/',
-  url: 'https://owlee-api-test.azurewebsites.net/',
-  webUrl:'https://owlee-test.azurewebsites.net/'
+  apiUrl: 'https://owlee-api.azurewebsites.net/api/',
+  url: 'https://owlee-api.azurewebsites.net/',
+  webUrl:'https://owlee.tech/'
 };
